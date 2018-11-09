61420878ae441170b8b233a9b875d99535746dfb
Britt <bmaewright@gmail.com>
2018-11-09 10:32:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// set container as element
+// var proone = document.getElementById("project-one");
+
+//when hover over project
+// proone.addEventListener("mouseover", showName);
+
+//Function showing name of project
+// function showName() {
+//   proone.innerText = "Project 1";
+// }
